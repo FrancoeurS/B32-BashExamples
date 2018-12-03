95745e333744c32ca70b8430b4c514121f025c89
Sacha Francoeur <sfrancoeur99@gmail.com>
2018-12-03 10:09:42 -0500

    Done with calculations

--- 03-calculs.sh
+++ 03-calculs.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
 
+# Pour vraiment exécuter ça prend un signe de dollar
+echo 10+19
+# Fait un calcul pis un echo de ça
+echo $((10+19))
+
+# Pour rentrer les chiffres souhaités
+read -p "Nombre 1 :" premier
+read -p "Nombre 2 :" second
+
+# Pour assigner quelque chose à résultat
+let resultat=$premier*$second
+
+echo $resultat
+
+
+
