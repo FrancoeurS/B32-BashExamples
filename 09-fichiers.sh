b2d838159d7c71fcd45ef49b5cfce767350bc4b9
Sacha Francoeur <sfrancoeur99@gmail.com>
2018-12-03 11:16:50 -0500

    Done with files

--- 09-fichiers.sh
+++ 09-fichiers.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+
+# -e = Pour voir si il existe
+# -d = Pour voir si il est un répertoire
+
+
+if test -e $1
+then
+	echo "existe"
+
+	if test -d $1
+	then
+		echo "Il est un répertoire"
+	fi
+else
+	echo "inexistant"
+fi
+
