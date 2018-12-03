bf721262a8841fc525965e99d7c56744309ff02d
Sacha Francoeur <sfrancoeur99@gmail.com>
2018-12-03 10:02:19 -0500

    Done with variables

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,2 +1,24 @@
 #!/bin/bash
 
+#Convention CamelMix , la variable commence tjrs par une miniscule
+# nomDeFamille= "a l l o"
+
+#Ne pas mettre des espaces entre égale
+prenom=KimCharles
+echo $prenom
+
+# Forcer une déclaration d'une variable "int"
+declare -i age=33
+
+# Mettre une variable READ ONLY (-r) (Comme une constante en C++)
+declare -r age2=33
+
+# Les variables systèmes (echo $0 - Le nom de l'exécutable)
+echo $0
+
+# Le premier argument utiliser dans la fonction, le deuxième et ainsi de suite
+echo $1
+echo $2
+
+# Pour savoir combien, il y a d'arguments
+echo $#
