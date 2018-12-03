4077bae73457c7cc71702a1cb85604c7bc3bc113
Sacha Francoeur <sfrancoeur99@gmail.com>
2018-12-03 11:10:30 -0500

    Done with regexp

--- 08-regexp.sh
+++ 08-regexp.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 
+echo -n "Texte : "
+read texte
+
+# $ = fin de
+# ^ = début de
+# + = Tout ce qui est précisé plus (une fois ou plus la pattern d'avant)
+if [[ $texte =~ ^[0-9]+$ ]]; then
+	echo "Numérique"
+fi
+
