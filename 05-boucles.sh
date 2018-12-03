86592f5723e35ca499e0acc5b8b0aee489bc8346
Sacha Francoeur <sfrancoeur99@gmail.com>
2018-12-03 10:51:38 -0500

    Done with loops

--- 05-boucles.sh
+++ 05-boucles.sh
@@ -1,2 +1,29 @@
 #!/bin/bash
 
+for ((i=0;i<5;i++))
+do
+	echo $i
+done
+
+# Exemple pour fichier dans répertoire
+
+liste=`ls`
+
+# For each , mettre les éléments de liste dans fic
+# Pour chacun des éléments de la liste, fais...
+for fic in $liste
+do
+	echo $fic
+done
+
+# -o = or
+# -a = and
+
+resultat=o
+
+while test $resultat = o -o $resultat = o
+do
+	read -p "Entrez o pour répéter la boucle : " resultat
+done
+
+
