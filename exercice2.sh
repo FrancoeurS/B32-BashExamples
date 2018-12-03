45c4f475371f21bae7ed74c726de6702a2f0b822
Sacha Francoeur <sfrancoeur99@gmail.com>
2018-12-03 12:14:12 -0500

    Done with exercice2

--- exercice2.sh
+++ exercice2.sh
@@ -1,1 +1,32 @@
 #!/bin/bash
+
+
+
+
+menu()	{
+	clear
+	echo ""
+	echo "==== CVM SOFT==="
+	echo "|    d = dict  |"
+	echo "|    q = quit  |"
+	echo "----------------"
+	echo ""
+	echo ""
+}
+
+menu
+read -p "Choix : " choix
+
+
+clear
+
+if [[ $choix = "d" ]];then
+	./exercice1.sh
+	sleep 1
+elif [[ $choix = "q" ]]
+then
+	echo "Vous avez quitter"
+fi
+
+clear
+
