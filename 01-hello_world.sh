e5249400a8540ce72aac4babd89e4ca0c5295762
Sacha Francoeur <sfrancoeur99@gmail.com>
2018-12-03 09:49:35 -0500

    Done with hello world

--- 01-hello_world.sh
+++ 01-hello_world.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
 
+# Ceci est un commentaire
+echo "Salut"
+
+# -n = empêche retour de ligne
+
+echo -n "Vive "
+echo "Linux"
+
+# -e =  affichage des caractères spéciaux
+echo -e "123\n123\n123"
+
+# read est comme le cin et p est la spécification
+# $ = quand on veut avoir la valeur d'une variable on place un signe de $
+read -p "Vous avez quel âge?" age
+echo $age "ans! Vous paraissez plus jeune"
